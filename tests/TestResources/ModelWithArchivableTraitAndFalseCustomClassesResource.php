183d612c769e2ceac296bb661c79c314e53a4b07
Jean Lalande <jeanlalande@gmail.com>
2026-02-04 08:54:40 -0500

    fix form method signature in test resources

--- tests/TestResources/ModelWithArchivableTraitAndFalseCustomClassesResource.php
+++ tests/TestResources/ModelWithArchivableTraitAndFalseCustomClassesResource.php
@@ -4,8 +4,8 @@ namespace Okeonline\FilamentArchivable\Tests\TestResources;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Okeonline\FilamentArchivable\Tables\Actions\ArchiveAction;
@@ -20,7 +20,7 @@ class ModelWithArchivableTraitAndFalseCustomClassesResource extends Resource
 
     public static ?string $modelLabel = 'with-classes';
 
-    public static function form(Form $form): Form
+    public static function form(Schema $form): Schema
     {
         return $form
             ->schema([
--- tests/TestResources/ModelWithArchivableTraitResource.php
+++ tests/TestResources/ModelWithArchivableTraitResource.php
@@ -4,8 +4,8 @@ namespace Okeonline\FilamentArchivable\Tests\TestResources;
 
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Form;
 use Filament\Resources\Resource;
+use Filament\Schemas\Schema;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
@@ -24,7 +24,7 @@ class ModelWithArchivableTraitResource extends Resource
 
     public static ?string $modelLabel = 'with';
 
-    public static function form(Form $form): Form
+    public static function form(Schema $form): Schema
     {
         return $form
             ->schema([
